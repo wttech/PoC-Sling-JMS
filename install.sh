4fc4b861b076f4a7b4fa3cdc1fcce1f0f968b7af
Tomasz Rękawek <trekawek@gmail.com>
2013-07-14 00:37:05 +0200

    removed custom install script

--- install.sh
+++
@@ -1,7 +0,0 @@
-mvn clean install
-for x in sling-activemq-*
-do
-mvn sling:install -Dsling.url=http://debian:4502
-mvn sling:install -Dsling.url=http://debian:4503
-mvn sling:install -Dsling.url=http://debian:4504
-done
