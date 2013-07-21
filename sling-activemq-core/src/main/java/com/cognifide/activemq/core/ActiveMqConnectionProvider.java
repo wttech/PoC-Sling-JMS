5809342e6c24b65bd1e0e01241bd63d2e556f338
Tomasz Rękawek <trekawek@gmail.com>
2013-07-21 12:35:40 +0200

    added embedded broker

--- sling-activemq-core/src/main/java/com/cognifide/activemq/core/ActiveMqConnectionProvider.java
+++ sling-activemq-core/src/main/java/com/cognifide/activemq/core/ActiveMqConnectionProvider.java
@@ -1,6 +1,6 @@
 package com.cognifide.activemq.core;
 
-import java.util.Dictionary;
+import java.util.Map;
 
 import javax.jms.JMSException;
 
@@ -13,7 +13,6 @@ import org.apache.felix.scr.annotations.Property;
 import org.apache.felix.scr.annotations.Reference;
 import org.apache.felix.scr.annotations.Service;
 import org.apache.sling.commons.osgi.PropertiesUtil;
-import org.osgi.service.component.ComponentContext;
 
 import com.cognifide.activemq.core.blob.SlingBlobServlet;
 import com.cognifide.activemq.core.blob.SlingBlobTransferPolicy;
@@ -21,8 +20,8 @@ import com.cognifide.jms.api.JmsConnectionProvider;
 
 @Component(immediate = true, metatype = false)
 @Service
-@Properties({ @Property(name = ActiveMqConnectionProvider.ACTIVEMQ_URL_PROPERTY_NAME, value = ActiveMqConnectionProvider.ACTIVEMQ_URL_DEFAULT, label = "ActiveMQ URL") })
-public class ActiveMqConnectionProvider implements JmsConnectionProvider {
+@Properties({ @Property(name = ActiveMQConnectionProvider.ACTIVEMQ_URL_PROPERTY_NAME, value = ActiveMQConnectionProvider.ACTIVEMQ_URL_DEFAULT, label = "ActiveMQ URL") })
+public class ActiveMQConnectionProvider implements JmsConnectionProvider {
 
 	protected static final String ACTIVEMQ_URL_PROPERTY_NAME = "activeMqUrl";
 
@@ -34,8 +33,7 @@ public class ActiveMqConnectionProvider implements JmsConnectionProvider {
 	private SlingBlobServlet blobServlet;
 
 	@Activate
-	protected void activate(ComponentContext context) {
-		Dictionary<?, ?> config = context.getProperties();
+	protected void activate(Map<String, Object> config) {
 		String url = PropertiesUtil.toString(config.get(ACTIVEMQ_URL_PROPERTY_NAME), ACTIVEMQ_URL_DEFAULT);
 		connectionFactory = new ActiveMQConnectionFactory(url);
 		connectionFactory.setBlobTransferPolicy(new SlingBlobTransferPolicy(blobServlet));
