a34cbd362fc65ec37ab04d363e5aea8ed5781b4f
Tomasz Rękawek <trekawek@gmail.com>
2013-07-23 19:59:55 +0200

    display instance data in shared session demo

--- sling-activemq-sandbox/src/main/java/com/cognifide/jms/sandbox/SharedSessionServlet.java
+++ sling-activemq-sandbox/src/main/java/com/cognifide/jms/sandbox/SharedSessionServlet.java
@@ -3,21 +3,29 @@ package com.cognifide.jms.sandbox;
 import java.io.PrintWriter;
 import java.util.Date;
 import java.util.Enumeration;
+import java.util.Random;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpSession;
 
 import org.apache.felix.scr.annotations.Component;
+import org.apache.felix.scr.annotations.Reference;
 import org.apache.felix.scr.annotations.sling.SlingServlet;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.SlingHttpServletResponse;
 import org.apache.sling.api.servlets.SlingSafeMethodsServlet;
+import org.apache.sling.settings.SlingSettingsService;
 
 @Component(immediate = true, metatype = false)
 @SlingServlet(paths = "/bin/cognifide/session", extensions = "txt", generateService = true, generateComponent = false)
 public class SharedSessionServlet extends SlingSafeMethodsServlet {
 
 	private static final long serialVersionUID = -7005192596672482278L;
+
+	private static final Random RANDOM = new Random();
+
+	@Reference
+	private SlingSettingsService settingsService;
 
 	@Override
 	public void doGet(SlingHttpServletRequest request, SlingHttpServletResponse response)
@@ -26,9 +34,12 @@ public class SharedSessionServlet extends SlingSafeMethodsServlet {
 		try {
 			HttpSession session = request.getSession();
 			if ("/add".equals(suffix)) {
-				session.setAttribute(new Date().toString(), new Value());
+				session.setAttribute(new Date().toString() + " " + RANDOM.nextInt(100), new Value());
 			}
 			PrintWriter writer = response.getWriter();
+			writer.append("Instance id: ").append(settingsService.getSlingId()).append('\n');
+			writer.append("Run modes: ").append(settingsService.getRunModes().toString()).append('\n');
+			writer.append("Path: ").append(settingsService.getSlingHomePath()).append("\n\n---\n");
 			@SuppressWarnings("unchecked")
 			Enumeration<String> names = session.getAttributeNames();
 			while (names.hasMoreElements()) {
